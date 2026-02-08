ce4bdfbae752ea8c1d6552d95d9835c3c234de36
abdullhx <116509406+abdullhx@users.noreply.github.com>
2026-02-08 17:48:01 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,5 +3,5 @@ local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
 
 -- تحميل الواجهة
 loadstring(game:HttpGet(
-    "https://raw.githubusercontent.com/abdullhx/A7xDev/main/ui/window.lua"
+    "https://raw.githubusercontent.com/abdullhx/A7xDev-Farm-Free/refs/heads/main/ui/window.lua"
 ))(Rayfield)
