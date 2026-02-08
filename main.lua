51c0712302c7f148cf5998c36011c9f602b1157a
abdullhx <116509406+abdullhx@users.noreply.github.com>
2026-02-08 17:38:41 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,9 @@
+-- تحميل Rayfield
+local Rayfield = loadstring(game:HttpGet(
+    "https://raw.githubusercontent.com/shlexware/Rayfield/main/source"
+))()
+
+-- تحميل الواجهة
+loadstring(game:HttpGet(
+    "https://raw.githubusercontent.com/abdullhx/A7xDev/main/ui/window.lua"
+))(Rayfield)
