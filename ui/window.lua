d0da5896a253477abee83d6f10ca62203a197463
abdullhx <116509406+abdullhx@users.noreply.github.com>
2026-02-10 17:30:32 +0300

    Update window.lua

--- ui/window.lua
+++ ui/window.lua
@@ -1,7 +1,4 @@
 return function(Rayfield)
-    local Players = game:GetService("Players")
-    local RunService = game:GetService("RunService")
-    local LocalPlayer = Players.LocalPlayer
 
     local Window = Rayfield:CreateWindow({
         Name = "A7xDev",
@@ -10,23 +7,8 @@ return function(Rayfield)
         KeySystem = false
     })
 
-    local Page1 = Window:CreateTab("Page 1")
+    local Tab = Window:CreateTab("Page 1")
 
-    -- ترحيب باسم اللاعب
-    Page1:CreateLabel("Welcome " .. LocalPlayer.Name .. " 👋🏻")
+    Tab:CreateLabel("IF YOU SEE THIS, UI WORKS ✅")
 
-    -- عرض الأموال
-    local MoneyLabel = Page1:CreateLabel("Your Money = Loading...")
-
-    -- تحديث مستمر للأموال
-    RunService.RenderStepped:Connect(function()
-        local money = "N/A"
-        local ok, val = pcall(function()
-            return LocalPlayer.PlayerGui.ATM.Main.Bank.Value
-        end)
-        if ok then money = tostring(val) end
-        MoneyLabel:Set("Your Money = " .. money)
-    end)
-
-    return Window
 end
